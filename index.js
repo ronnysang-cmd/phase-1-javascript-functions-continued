d2c6aaa0fef165bbf4d6219621fd804ab697f7aa
ronnysang <ronny.sang@student.moringaschool.com>
2025-09-08 14:52:07 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(activity = "go to the office") {
+  return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(flair = "*") {
+  return function(adjective = "special") {
+    return `You are ${flair}${adjective}${flair}!`;
+  };
+}
+
+const Calculator = {
+  add: function(a, b) {
+    return a + b;
+  },
+  subtract: function(a, b) {
+    return a - b;
+  },
+  multiply: function(a, b) {
+    return a * b;
+  },
+  divide: function(a, b) {
+    return a / b;
+  }
+};
+
+function actionApplyer(startingInt, functionsArray) {
+  let result = startingInt;
+  for (let func of functionsArray) {
+    result = func(result);
+  }
+  return result;
+}
